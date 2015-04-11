da909828c14b541fba4ba9a078a7499480905b3c
YiChenshu <ycswaves@gmail.com>
2015-04-11 13:52:41 +0800

    Initial Commit

---
+++ SoundShake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundShake
+//
+//  Created by YCS on 11/4/15.
+//  Copyright (c) 2015 ycswaves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
